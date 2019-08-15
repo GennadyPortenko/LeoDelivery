c57046d3a2aeac119c995f2e05cbabf7db8b347b
Gennady Portenko <gportenko@gmail.com>
2019-08-15 16:14:25 +0400

    added localization support for english and french with simple example for /regular/home

--- src/main/java/com/cmdelivery/config/AppConfig.java
+++ src/main/java/com/cmdelivery/config/AppConfig.java
@@ -3,6 +3,11 @@ package com.cmdelivery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class AppConfig {
@@ -10,4 +15,7 @@ public class AppConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
 }
